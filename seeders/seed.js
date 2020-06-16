263d47dd2a35da51eb608590ca2f5e93182db85f
Andrew Murphy <amurphy37@gmail.com>
2020-06-16 16:31:38 -0400

    testing heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+const MONGODB_URI = process.env.MONGODB_URL
 
 const options = {
   useNewURLParser: true,
